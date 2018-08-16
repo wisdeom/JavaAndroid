814a0e9635eb1624ff2efd9bb54349e52fef4941
Jihye Eom <jeom327@outlook.com>
2018-08-16 13:54:05 +0900

    INITIAL checkin

---
+++ Droid/obj/Debug/android/src/md5a5cfb4e338a6be57ab73f714f654876c/BrowseItemsAdapter.java
@@ -0,0 +1,65 @@
+package md5a5cfb4e338a6be57ab73f714f654876c;
+
+
+public class BrowseItemsAdapter
+	extends md5a5cfb4e338a6be57ab73f714f654876c.BaseRecycleViewAdapter
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreateViewHolder:(Landroid/view/ViewGroup;I)Landroid/support/v7/widget/RecyclerView$ViewHolder;:GetOnCreateViewHolder_Landroid_view_ViewGroup_IHandler\n" +
+			"n_onBindViewHolder:(Landroid/support/v7/widget/RecyclerView$ViewHolder;I)V:GetOnBindViewHolder_Landroid_support_v7_widget_RecyclerView_ViewHolder_IHandler\n" +
+			"n_getItemCount:()I:GetGetItemCountHandler\n" +
+			"";
+		mono.android.Runtime.register ("JavaTest.Droid.BrowseItemsAdapter, JavaTest.Droid", BrowseItemsAdapter.class, __md_methods);
+	}
+
+
+	public BrowseItemsAdapter ()
+	{
+		super ();
+		if (getClass () == BrowseItemsAdapter.class)
+			mono.android.TypeManager.Activate ("JavaTest.Droid.BrowseItemsAdapter, JavaTest.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public android.support.v7.widget.RecyclerView.ViewHolder onCreateViewHolder (android.view.ViewGroup p0, int p1)
+	{
+		return n_onCreateViewHolder (p0, p1);
+	}
+
+	private native android.support.v7.widget.RecyclerView.ViewHolder n_onCreateViewHolder (android.view.ViewGroup p0, int p1);
+
+
+	public void onBindViewHolder (android.support.v7.widget.RecyclerView.ViewHolder p0, int p1)
+	{
+		n_onBindViewHolder (p0, p1);
+	}
+
+	private native void n_onBindViewHolder (android.support.v7.widget.RecyclerView.ViewHolder p0, int p1);
+
+
+	public int getItemCount ()
+	{
+		return n_getItemCount ();
+	}
+
+	private native int n_getItemCount ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Droid/obj/Debug/android/src/md5a5cfb4e338a6be57ab73f714f654876c/SplashActivity.java
@@ -0,0 +1,47 @@
+package md5a5cfb4e338a6be57ab73f714f654876c;
+
+
+public class SplashActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("JavaTest.Droid.SplashActivity, JavaTest.Droid", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("JavaTest.Droid.SplashActivity, JavaTest.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
